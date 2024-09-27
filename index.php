7a8fa61727049df93ca6b6e38b48dd532ab867fb
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-27 08:54:12 +0200

    prova stampa in pagina con variabili php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+        $nome = 'Michele';
+        $cognome = 'Bonanno';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <h1>
+            Ciao Mondo
+        </h1>
+
+        <p>
+            Io sono <?php echo $nome $cognome ?>
+        </p>
+</body>
+</html>
